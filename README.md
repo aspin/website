65188c66e3ede034468b1e3dade3b1a91c97bbe3
aspin <tsunami70875@gmail.com>
2014-11-20 18:13:25 -0600

    changes

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ to do:
 
 * fading / appearing menu bar (see luluspice)
 * make images
+* beginning banner photo: starts blank - slider to the right, half done, slider ot the left, complete (as if it's being painted / drawn)
--- client/views/home.js
+++ client/views/home.js
@@ -1,3 +1,24 @@
 Template.home.rendered = function () {
-    
+    // $("body").css("display", "none");
+    // $("body").fadeIn(2000);
+
+    $(window).scroll(function() {
+        if ($(".cd-header").offset().top > 50) {
+            $(".cd-header").addClass("cd-header-active");
+        } else {
+            $(".cd-header").removeClass("cd-header-active");
+        }
+    });
 };
+
+Template.home.events({
+    'click .page-nav': function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        $('html, body').stop().animate({
+            scrollTop: $($(e.currentTarget).attr("href")).offset().top},
+            1500, 'easeInOutExpo');
+        $(e.currentTarget).attr("href");
+    }
+});
