e6db545af48b85a3c1dc3fa6ecab41a648145cc1
Kevin Chen <kevin@bloxroute.com>
2023-03-01 12:18:20 -0800

    setup inital libraries

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Container } from "@mui/material"
+
+function App() {
+    return (
+        <Container>
+            <h1>my app</h1>
+        </Container>
+    )
+}
+
+export default App
