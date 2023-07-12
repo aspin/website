249730786b62cbceeb9ad0ecbfeed27385dbdf83
Kevin Chen <kevinjchen94@gmail.com>
2023-07-12 00:10:13 -0400

    deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+IP="45.55.69.255"
+
+rm -rf dist
 yarn build
-scp -r dist/* root@45.55.69.255:/www/kevinchen/
-scp -r resume.pdf root@45.55.69.255:/www/kevinchen/resume.pdf
+
+ssh root@$IP "rm -rf /www/kevinchen/*"
+scp -r dist/* root@$IP:/www/kevinchen/
+scp -r resume.pdf root@$IP:/www/kevinchen/resume.pdf
